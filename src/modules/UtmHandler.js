ec2a6b99edf5b10ba17879dc4840544286eeed62
cihusss <cihusss@gmail.com>
2023-12-22 18:01:29 +0200

    utm handler logic

---
+++ src/modules/UtmHandler.js
@@ -0,0 +1,42 @@
+export const UtmHandler = (a, b, c) => {
+  // const { utmMediumParam, utmMediumStorage } = utmData;
+
+  console.log(`${c}:`, a);
+  console.log('localStorage:', b);
+
+  if (!a && !b) {
+    console.log('BOTH NULL > doing nothing');
+  } else if (a && !b) {
+    localStorage.setItem(c, a);
+    console.log('utm HERE > storing UTM');
+  } else if (!a && b) {
+    console.log('localStorage HERE > doing nothing');
+  } else if (a && b) {
+    if (a !== b) {
+      localStorage.setItem(c, a);
+      console.log('BOTH HERE > utm DIFFERENT from localStorage > storing new utm');
+    } else {
+      console.log('BOTH HERE and MATCHING > doing nothing');
+    }
+  }
+
+  // if (!utmMediumParam && !utmMediumStorage) {
+  //   console.log('BOTH NULL > doing nothing');
+  // }
+  // else if (utmMediumParam && !utmMediumStorage) {
+  //   localStorage.setItem('utm_medium', utmMediumParam);
+  //   console.log('UTM HERE > storing UTM');
+  // }
+  // else if (!utmMediumParam && utmMediumStorage) {
+  //   console.log('STORAGE HERE > doing nothing');
+  // }
+  // else if (utmMediumParam && utmMediumStorage) {
+  //   if (utmMediumParam !== utmMediumStorage) {
+  //     localStorage.setItem('utm_medium', utmMediumParam);
+  //     console.log('BOTH HERE > UTM DIFFERENT from STORAGE > storing new UTM');
+  //   }
+  //   else {
+  //     console.log('BOTH HERE and MATCHING > doing nothing');
+  //   }
+  // }
+};
--- src/modules/UtmManager.js
+++ src/modules/UtmManager.js
@@ -1,40 +1,28 @@
+import {UtmHandler} from './UtmHandler';
+
 export const UtmManager = () => {
   // Get URL params
   const searchParams = new URLSearchParams(window.location.search);
-  const utmMediumParam = searchParams.get('utm_medium');
-  // const utmCampaignParam = searchParams.get('utm_campaign');
+  const utmData = {};
 
-  // Get local storage values
-  let utmMediumStorage = localStorage.getItem('utm_medium');
-  // let utmCampaignStorage = localStorage.getItem('utm_campaign');
+  utmData.medium = {
+    utm: searchParams.get('utm_medium'),
+    localStorage: localStorage.getItem('utm_medium'),
+    label: 'utm_medium',
+  };
 
-  console.log('utmMediumParam:', utmMediumParam);
-  // console.log('utmCampaignParam:', utmCampaignParam);
-  console.log('utmMediumStorage:', utmMediumStorage);
-  // console.log('utmCampaignStorage:', utmCampaignStorage);
+  utmData.campaign = {
+    utm: searchParams.get('utm_campaign'),
+    localStorage: localStorage.getItem('utm_campaign'),
+    label: 'utm_campaign',
+  };
 
-  if (!utmMediumParam && !utmMediumStorage) {
-    console.log('BOTH NULL > doing nothing');
-  }
-  else if (utmMediumParam && !utmMediumStorage) {
-    localStorage.setItem('utm_medium', utmMediumParam);
-    console.log('UTM HERE > storing UTM');
-  }
-  else if (!utmMediumParam && utmMediumStorage) {
-    console.log('STORAGE HERE > doing nothing');
-  }
-  else if (utmMediumParam && utmMediumStorage) {
-    if (utmMediumParam !== utmMediumStorage) {
-      localStorage.setItem('utm_medium', utmMediumParam);
-      console.log('BOTH HERE > UTM DIFFERENT from STORAGE > storing new UTM');
-    }
-    else {
-      console.log('BOTH HERE and MATCHING > doing nothing');
+  console.log('utmData:', utmData);
+
+  for (let key in utmData) {
+    console.log(`------------------------->`)
+    if (utmData[key].utm) {
+      UtmHandler(utmData[key].utm, utmData[key].localStorage, utmData[key].label);
     }
   }
-  //
-  // if (utmCampaignStorage == null) {
-  //   localStorage.setItem('utm_campaign_local_storage', utmCampaignParam);
-  //   console.log('utm_campaign is == null');
-  // }
 };
