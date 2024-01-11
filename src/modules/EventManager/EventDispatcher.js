591c59b539d5cd5c4fca7e71ee16d26f4a7a215e
cihusss <cihusss@gmail.com>
2024-01-11 18:54:25 +0200

    marketo events test

--- src/modules/EventManager/EventDispatcher.js
+++ src/modules/EventManager/EventDispatcher.js
@@ -3,5 +3,11 @@ import { mainData, sendToServer } from '../DataProcessor/DataProcessor';
 export const EventDispatcher = (e) => {
   // console.log('eventName:', e);
   mainData.event = e;
-  sendToServer();
+  // sendToServer();
+
+  MktoForms2.whenReady(function (form) {
+    form.onSuccess(function (values, followUpUrl) {
+      sendToServer();
+    });
+  });
 };
