5c94f3e9a7f8404440e7817d90816802f280d9af
cihusss <cihusss@gmail.com>
2024-01-11 19:22:54 +0200

    formVals fix 03

--- src/modules/DataProcessor/DataProcessor.js
+++ src/modules/DataProcessor/DataProcessor.js
@@ -7,14 +7,14 @@ export const getUserAgent = () => {
 
 export const sendToServer = () => {
   console.log(`\nSEND TO SERVER ------------------------->`);
-  console.log(mainData);
+  console.dir(mainData);
 };
 
 export const getFormData = (e) => {
   MktoForms2.whenReady(function (form) {
     form.onSuccess(function (values, followUpUrl) {
       const formVals = form.vals();
-      console.log('formVals: ', formVals);
+      // console.log('formVals: ', formVals);
       mainData.formData = formVals;
       sendToServer();
     });
